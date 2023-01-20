ef6b003bd6d41b06e2fe1202e49d7c4a81cfd0be
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-19 19:07:11 -0500

    FIx: Se agregan los atributos correspondientes a cada entidad

--- domain/src/main/java/co/com/ausencia/entidades/Clasificacion.java
+++ domain/src/main/java/co/com/ausencia/entidades/Clasificacion.java
@@ -1,10 +1,16 @@
 package co.com.ausencia.entidades;
 
+import co.com.ausencia.valor.Descripcion;
 import co.com.ausencia.valor.IdClasificacion;
+import co.com.ausencia.valor.Tipo;
 import co.com.sofka.domain.generic.Entity;
 
 public class Clasificacion extends Entity<IdClasificacion> {
-    public Clasificacion(IdClasificacion entityId) {
+    private Descripcion descripcion;
+    private Tipo tipo;
+    public Clasificacion(IdClasificacion entityId, Descripcion descripcion, Tipo tipo) {
         super(entityId);
+        this.descripcion = descripcion;
+        this.tipo = tipo;
     }
 }
--- domain/src/main/java/co/com/ausencia/entidades/Registro.java
+++ domain/src/main/java/co/com/ausencia/entidades/Registro.java
@@ -1,11 +1,16 @@
 package co.com.ausencia.entidades;
 
+import co.com.ausencia.valor.Descripcion;
 import co.com.ausencia.valor.IdRegistro;
+import co.com.ausencia.valor.Tipo;
 import co.com.sofka.domain.generic.Entity;
 
 public class Registro extends Entity<IdRegistro> {
-
-    public Registro(IdRegistro entityId) {
+    private Descripcion descripcion;
+    private Tipo tipo;
+    public Registro(IdRegistro entityId, Descripcion descripcion, Tipo tipo) {
         super(entityId);
+        this.descripcion = descripcion;
+        this.tipo = tipo;
     }
 }
