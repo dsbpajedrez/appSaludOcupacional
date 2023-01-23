a5619ac359ad3d55a9482afcbdc05812588ebfe5
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-01-23 12:46:08 -0500

    New: Otra forma de solucionar actualizar registro en ausencia test

--- negocio/src/test/java/co/com/ausencia/ActualizarRegistroUseCaseTest.java
+++ negocio/src/test/java/co/com/ausencia/ActualizarRegistroUseCaseTest.java
@@ -9,6 +9,7 @@ import co.com.ausencia.valor.*;
 import co.com.sofka.business.generic.UseCaseHandler;
 import co.com.sofka.business.repository.DomainEventRepository;
 import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -36,10 +37,11 @@ class ActualizarRegistroUseCaseTest {
         Fecha fecha = new Fecha(LocalDateTime.now());
         var command = new ActualizarRegistro(idAusencia,idRegistro,lugar,fecha);
 
-        when(repository.getEventsBy("1")).thenReturn(List.of(
+       /* when(repository.getEventsBy("1")).thenReturn(List.of(
                 new AusenciaAgregada(new Duracion(fecha.value())),
                 new RegistroAgregado(IdRegistro.of("2"),new Lugar("Escaleras"),new Fecha(LocalDateTime.now()))
-        ));
+        ));*/
+        when(repository.getEventsBy("1")).thenReturn(history());
         useCase.addRepository(repository);
 
 //        when
@@ -53,7 +55,7 @@ class ActualizarRegistroUseCaseTest {
         assertEquals("2", event.getIdRegistro().value());
         assertEquals("Escaleras", event.getLugar().value());
     }
-   /* private List<DomainEvent> history() {
+   private List<DomainEvent> history() {
         IdAusencia idAusencia = IdAusencia.of("1");
         Duracion duracion = new Duracion(LocalDateTime.now());
         var event = new AusenciaAgregada(duracion);
@@ -64,6 +66,6 @@ class ActualizarRegistroUseCaseTest {
         var event2 = new RegistroAgregado(idRegistro,lugar,fecha);
         event2.setAggregateRootId(idAusencia.value());
         return List.of(event, event2);
-    }*/
+    }
 
 }
